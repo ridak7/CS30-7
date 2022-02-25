3fb34b2d55bbd71472a8f151ef319bf90a33c007
904188001 <904188001@L815WD150038.edu.cbe.ab.ca>
2022-02-25 09:14:17 -0700

    created BusinessAcct

---
+++ Chapter 8/src/BusinessAcct.java
@@ -0,0 +1,8 @@
+
+public class BusinessAcct {
+
+	public BusinessAcct() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Chapter 8/src/PersonalAcct.java
+++ Chapter 8/src/PersonalAcct.java
@@ -2,7 +2,7 @@
 public class PersonalAcct {
 
 	public PersonalAcct() {
-		// TODO Auto-generated constructor stub
+	
 	}
 
 }
